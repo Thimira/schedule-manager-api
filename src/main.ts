c4f84d6e53cb54b73d2b2a98156da4c4bce669db
Thimira Amaratunga <thimiraamara@gmail.com>
2025-01-01 10:25:56 +0000

    adding filters to handle uncaught exceptions globally

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -7,6 +8,9 @@ async function bootstrap() {
 
   // Enable global validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // Enable global filter
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }
